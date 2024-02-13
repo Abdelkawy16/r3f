22659ec9e650039dac67b72f6cff48d23982cbad
Mustafa Abdelkawy <32801595+Abdelkawy16@users.noreply.github.com>
2024-02-13 18:37:43 +0200

    adding controls

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,38 +1,62 @@
-import { useFrame } from "@react-three/fiber";
-import { useRef, useState } from "react";
+import { useFrame, extend, useThree } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { useRef } from "react";
 import * as THREE from "three";
+import CustomObject from "./custom-object";
+
+extend({ OrbitControls });
 
 export default function Experience() {
   //   const [cubeRotation, setCubeRotation] = useState(Math.PI / 4);
   const cubeRef = useRef<any>();
   const groupRef = useRef<any>();
+  const { camera, gl } = useThree();
+
   useFrame((state, delta) => {
-    console.log("tick");
+    const angle = state.clock.elapsedTime;
+
+    // state.camera.position.x = Math.sin(angle) * 8;
+    // state.camera.position.z = Math.cos(angle) * 8;
+    // state.camera.lookAt(new THREE.Vector3());
     // setCubeRotation(cubeRotation + 0.01);
     cubeRef.current.rotation.y += delta;
-    groupRef.current.rotation.y += delta;
+    // groupRef.current.rotation.y += delta;
   });
 
   return (
     <>
-    <group ref={groupRef}>
-      <mesh ref={cubeRef} rotation-y={Math.PI / 4} position-x={2} scale={1.5}>
-        <boxGeometry />
-        <meshBasicMaterial color="mediumpurple" />
-      </mesh>
+      <orbitControls args={[camera, gl.domElement]} />
 
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshBasicMaterial color="orange" />
-      </mesh>
-    </group>
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
 
-      <mesh rotation-x={-Math.PI / 2} position-y={-1}>
+      <ambientLight intensity={1.5} />
+
+      <group ref={groupRef}>
+        <mesh
+          ref={cubeRef}
+          rotation-y={Math.PI / 4}
+          position-x={2}
+          scale={1.5}
+          castShadow
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh position-x={-2} castShadow>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+      </group>
+
+      <mesh rotation-x={-Math.PI / 2} position-y={-1} castShadow receiveShadow>
         <planeGeometry args={[20, 20, 8, 8]} />
-        <meshBasicMaterial
+        <meshStandardMaterial
           args={[{ color: "greenyellow", side: THREE.DoubleSide }]}
         />
       </mesh>
+
+      <CustomObject />
     </>
   );
 }
--- src/components/mesh-component.tsx
+++ src/components/mesh-component.tsx
@@ -2,15 +2,25 @@
 
 import { Canvas } from "@react-three/fiber";
 import Experience from "./experience";
-import { OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
+// import { OrbitControls } from "@react-three/drei";
 
 // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 export default function MeshComponent() {
   return (
     <>
-      <Canvas>
-        <OrbitControls />
+      <Canvas
+        // dpr={[1, 2]} default
+        gl={{
+          antialias: true,
+          toneMapping: THREE.CineonToneMapping,
+          // outputEncoding: THREE.LinearEncoding,
+          outputEncoding: THREE.sRGBEncoding,
+        }}
+        camera={{ fov: 45, near: 0.1, far: 200, position: [3, 2, 6] }}
+      >
+        {/* <OrbitControls /> */}
         <Experience />
       </Canvas>
     </>
